0f3bfb2e74134605cf732fac0a49e8fd137e415f
Lauv <angelalauvis@gmail.com>
2024-01-31 18:17:28 +0700

    project snippets: server actions, form and caching in nextjs

---
+++ hands-on/snippets/src/actions/index.ts
@@ -0,0 +1,63 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { db } from "@/db";
+
+export async function editSnippet(id: number, code: string) {
+    await db.snippet.update({
+        where: { id },
+        data: { code },
+    });
+
+    revalidatePath(`/snippets/${id}`);
+    redirect(`/snippets/${id}`);
+}
+
+export async function deleteSnippet(id: number) {
+    await db.snippet.delete({
+        where: { id },
+    });
+
+    redirect("/");
+}
+
+export async function createSnippet(
+    formState: { message: String },
+    formData: FormData
+) {
+    try {
+        const title = formData.get("title");
+        const code = formData.get("code");
+
+        if (typeof title !== "string" || title.length < 3) {
+            return {
+                message: "Title must be longer",
+            };
+        }
+        if (typeof code !== "string" || code.length < 10) {
+            return {
+                message: "Code must be longer",
+            };
+        }
+        // create a new record in the database
+        const snippet = await db.snippet.create({
+            data: {
+                title,
+                code,
+            },
+        });
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            return {
+                message: err.message,
+            };
+        } else {
+            return {
+                message: "Something went wrong ...",
+            };
+        }
+    }
+
+    redirect("/");
+}
