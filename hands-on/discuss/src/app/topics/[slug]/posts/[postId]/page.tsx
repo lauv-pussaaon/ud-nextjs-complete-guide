ed3614091fbade036f3e3a049355806697f74f1d
Lauv <angelalauvis@gmail.com>
2024-02-01 20:02:05 +0700

    discuss: handle loading with suspense element

--- hands-on/discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
+++ hands-on/discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import { Suspense } from "react";
 import PostShow from "@/components/posts/post-show";
+import PostShowLoading from "@/components/posts/post-show-loading";
 import CommentList from "@/components/comments/comment-list";
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
@@ -23,7 +25,9 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
             >
                 {"< "}Back to {slug}
             </Link>
-            <PostShow postId={postId} />
+            <Suspense fallback={<PostShowLoading />}>
+                <PostShow postId={postId} />
+            </Suspense>
             <CommentCreateForm postId={postId} startOpen />
             <CommentList postId={postId} />
         </div>
